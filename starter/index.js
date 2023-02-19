e1c3594584c90a4544af4d359364b4600a4c8c17
Natasha Okparavero <tasha.okparavero@yahoo.com>
2023-02-19 10:07:46 +0000

    added questions array

--- starter/index.js
+++ starter/index.js
@@ -5,7 +5,63 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        name: "Title",
+        message: "What is the name/title of your project?", // question for title section
+    },
+    {
+        type: "input",
+        name: "Description",
+        message: "Describe the purpose and functionality of this project", // question of description section
+    },
+    {
+        type: "input",
+        name: "screenshot",
+        message: "please provide the relative path to the screenshot of the application", // question for providing screenshot
+    },
+    {
+        type: "input",
+        name: "link",
+        message: "please provide the URL to your deployed site", // question for link to deployed site
+    },
+    {
+        type: "checkbox",
+        name: "license",
+        message: "please choose the license that you used for this project",
+        choices: ["MIT", "APACHE2.0", "Boost1.0", "MPL2.0", "BSD2", "BSD3", "none"],
+    }, // question to choose what license you have used 
+    {
+        type: "input",
+        name: "usage",
+        message: "What technologies are used in this project", // question for technologies used 
+    },
+    {
+        type: "input",
+        name: "features",
+        message: "List the top features of this project", // question for project features
+    },
+    {
+        type: "input",
+        name: "creator",
+    message: "What is your GitHub username.", // question for github username/repo
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Provide a valid email address.", // question for creators email
+      },
+      {
+        type: "input",
+        name: "contributors",
+        message: "Please list any contributors. (Use GitHub usernames)",
+        default: "", // question for anyone who had contributed to the project
+      },
+      {
+        type: "input",
+        name: "test",
+        message: "If applicable, what tests are required for this application", // question for tests available for this application
+      },
 ];
 
 // function to write README file
